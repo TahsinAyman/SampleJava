4fb2dcbbc836fc14ca3bc4f7a6158543d84a6816
TahsinAyman <mail4tahsin@gmail.com>
2023-07-07 13:36:42 +0600

    Initial Commit

---
+++ src/main/java/Sleep.java
@@ -0,0 +1,7 @@
+public class Sleep {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Start");
+        Thread.sleep(60 * 1000);
+        System.out.println("Sleep");
+    }
+}
